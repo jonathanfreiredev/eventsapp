826749b13203325a9b253abca0c13cf76269fb9e
Jonathan Freire <jonathanfreire@Mac.fritz.box>
2024-12-05 18:23:12 +0100

    Fix host ip on android

--- api/client.ts
+++ api/client.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
+import { Platform } from "react-native";
+
+const ip = Platform.OS === "android" ? "10.0.2.2" : "127.0.0.1";
 
 export const apiClient = axios.create({
-    baseURL: "http://127.0.0.1:4000",
+    baseURL: `http://${ip}:4000`,
     headers: {
         "Content-Type": "application/json",
     },
